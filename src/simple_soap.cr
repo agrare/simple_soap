ae4603a90956374222b51f4f5f3021ebe4529dbb
Adam Grare <agrare@redhat.com>
2017-07-19 16:39:34 -0400

    Pull out HTTP transport layer into a client

--- src/simple_soap.cr
+++ src/simple_soap.cr
@@ -1,58 +1,7 @@
 require "./simple_soap/*"
-require "http/client"
 
 class SimpleSoap
-  property cookie : String?
-  getter host : String
-  getter path : String
-  getter port : Int32
-  getter ssl : Bool
-  getter insecure : Bool
-  getter read_timeout : Int32
-  getter connect_timeout : Int32
-
-  @http : HTTP::Client?
-
-  def initialize(@host, @port, @path, @ssl = true, @insecure = false, @cookie = nil, @read_timeout = 1000000, @connect_timeout = 60)
-    @http = nil
-    restart_http
-  end
-
-  def close
-    @http.try &.close rescue IOError
-  end
-
-  def http : HTTP::Client
-    @http.not_nil!
-  end
-
-  def restart_http
-    begin
-      @http.try &.close
-    rescue ex
-      puts "WARNING: Ignoring exception: #{ex.message}"
-      puts ex.backtrace.join("\n")
-    end
-
-    tls = if ssl
-            if insecure
-              OpenSSL::SSL::Context::Client.insecure
-            else
-              # TODO: set certificate and key
-              OpenSSL::SSL::Context::Client.new
-            end
-          else
-            false
-          end
-
-    new_http = HTTP::Client.new(host, port, tls)
-    new_http.read_timeout = read_timeout
-    new_http.connect_timeout = connect_timeout
-
-    @http = new_http
-  end
-
-  def soap_envelope(body, header = nil)
+  def self.soap_envelope(header_proc = nil)
     namespaces = {
       "xmlns:xsd" => "http://www.w3.org/2001/XMLSchema",
       "xmlns:env" => "http://schemas.xmlsoap.org/soap/envelope/",
@@ -61,41 +10,31 @@ class SimpleSoap
 
     XML.build do |xml|
       xml.element("env:Envelope", namespaces) do
-        if header
+        if header_proc
           xml.element("env:Header") do
-            header.call(xml)
+            header_proc.call(xml)
           end
         end
 
         xml.element("env:Body") do
-          body.call(xml)
+          yield xml
         end
       end
     end
   end
 
-  def request(action, body)
+  def self.request(client, path, action, body, cookie = nil)
     headers = HTTP::Headers.new
     headers["content-type"] = "text/xml; charset=utf-8"
     headers["SOAPAction"] = action
-    if cookie = @cookie
-      headers["cookie"] = cookie
-    end
+    headers["cookie"] = cookie if cookie
 
-    begin
-      response = http.post(path, headers, body)
-    rescue ex
-      restart_http
-      raise ex
-    end
-
-    raise "Got HTTP 503: Service unavailable" if response.status_code == 503
-
-    self.cookie = response.headers["set-cookie"] if response.headers.has_key?("set-cookie")
+    response = client.post(path, headers, body)
+    raise "HTTP Error" if response.status_code != 200
 
     xml = XML.parse(response.body)
     node = xml.xpath_node("//soapenv:Body/*", {"soapenv" => "http://schemas.xmlsoap.org/soap/envelope/"})
 
-    [node, response.body.size]
+    {node, response}
   end
 end
